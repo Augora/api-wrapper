63a571628f19852adcd0fd10da1f9b469ed18195
kevinbacas <bacas.kevin@hotmail.fr>
2017-03-11 01:10:24 +0100

    Normalize output from API

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const utils = require('./Utils/APICall')
+const { providedNormalizeDeputes } = require('./Utils/Normalize')
 
 const deputes = utils.get('http://www.nosdeputes.fr/deputes/json')
 
 deputes
   .map(r => r.data)
-  .map(r => r.deputes)
-  .subscribe(r => console.log(r))
+  .map(d => Object.assign({}, {
+    deputes: d.deputes.map(i => Object.assign({}, i.depute))
+  }))
+  .map(d => providedNormalizeDeputes(d.deputes))
+  .subscribe(r => console.log(r.entities.depute['1']))

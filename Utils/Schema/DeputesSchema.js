717c9ac7165cb6470d20ed0527775d555bb8f03f
kevinbacas <bacas.kevin@hotmail.fr>
2017-03-11 01:10:13 +0100

    Add schemas and normalizing function

---
+++ Utils/Schema/DeputesSchema.js
@@ -0,0 +1,12 @@
+const { schema } = require('normalizr')
+
+const site = new schema.Entity('site', {}, {
+  idAttribute: 'site'
+})
+const depute = new schema.Entity('depute', {
+  sites_web: [ site ]
+})
+
+module.exports = {
+  depute
+}

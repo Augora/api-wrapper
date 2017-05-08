f9bead4e4bdcb099be97590771cab9bfc4e8ea4e
kevinbacas <bacas.kevin@hotmail.fr>
2017-05-08 12:14:30 +0200

    Updated deps and fixed tests

--- src/Utils/APICall.test.ts
+++ src/Utils/APICall.test.ts
@@ -1,8 +1,8 @@
-import utils from './APICall';
+import { promiseToObservable, getAsObservable } from './APICall';
 
 test('promiseToObservable calls the function with the right argument', () => {
   const fn = jest.fn();
-  utils.promiseToObservable(fn, 5);
+  promiseToObservable(fn, 5);
   expect(fn.mock.calls.length).toBe(1);
   expect(fn.mock.calls[0][0]).toBe(5);
 });
@@ -10,7 +10,7 @@ test('promiseToObservable calls the function with the right argument', () => {
 test('getAsObservable calls the functions with right arguments', () => {
   const axios = jest.fn();
   const promiseToObservable = jest.fn();
-  utils.getAsObservable(axios, promiseToObservable, 'a.fr', { a: 1 });
+  getAsObservable(axios, promiseToObservable, 'a.fr', { a: 1 });
   expect(axios.mock.calls.length).toBe(1);
   expect(axios.mock.calls[0][0]).toBe('a.fr');
   expect(axios.mock.calls[0][1]).toEqual({ a: 1 });

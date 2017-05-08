05cade447097b53509ea79dec8c3c8f9157808ef
kevinbacas <bacas.kevin@hotmail.fr>
2017-05-08 20:21:15 +0200

    Fix #4: add getDeputy method

--- src/Utils/Schema/DeputySchema.ts
+++ src/Utils/Schema/DeputySchema.ts
@@ -16,13 +16,21 @@ export const email = new schema.Entity('email', {}, {
   idAttribute: 'email',
 });
 
-export const deputyInOffice = new schema.Entity('depute', {
+export const responsabilite = new schema.Entity('responsabilite', {}, {
+  idAttribute: (value) => `${value.responsabilite.organisme}/${value.responsabilite.fonction}`,
+  processStrategy: (value) => {
+    return Object.assign({}, value.responsabilite);
+  },
+});
+
+export const deputy = new schema.Entity('depute', {
   sites_web: [site],
   adresses: [adresse],
   anciens_autres_mandats: [mandat],
   autres_mandats: [mandat],
   anciens_mandats: [mandat],
   emails: [email],
+  groupes_parlementaires: [responsabilite],
+  responsabilites: [responsabilite],
+  responsabilites_extra_parlementaires: [responsabilite],
 });
-
-export default deputyInOffice;

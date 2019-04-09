f56e28bf4e95df41fa43995a8fd50ec28e2ebf83
kevinbacas <bacas.kevin@hotmail.fr>
2019-04-10 00:13:02 +0200

    Removed normalizr, migrated mapping to a simpler version. Add tests

--- src/CoreAPI/Deputy.ts
+++ src/CoreAPI/Deputy.ts
@@ -1,10 +1,6 @@
 import { assign } from "lodash";
 import { getFromUrl } from "../Utils/APICall";
-import {
-  providedNormalizeDeputes,
-  providedNormalizeDeputesInOffice,
-  providedNormalizeDeputy
-} from "../Utils/Normalize";
+import { deputyAttributesMapping } from "./Mappings";
 
 interface IResponse {
   data: any;
@@ -22,24 +18,26 @@ export function getDeputies() {
   return getFromUrl("https://www.nosdeputes.fr/deputes/json")
     .then((r: IResponse) => r.data)
     .then((d: IDeputies) =>
-      d.deputes.map((i: IDeputyHolder) => assign({}, i.depute))
-    )
-    .then((d: {}) => providedNormalizeDeputes(d));
+      d.deputes.map((i: IDeputyHolder) =>
+        assign({}, deputyAttributesMapping(i.depute))
+      )
+    );
 }
 
 export function getDeputiesInOffice() {
   return getFromUrl("https://www.nosdeputes.fr/deputes/enmandat/json")
     .then((r: IResponse) => r.data)
     .then((d: IDeputies) =>
-      d.deputes.map((i: IDeputyHolder) => assign({}, i.depute))
-    )
-    .then((d: {}) => providedNormalizeDeputesInOffice(d));
+      d.deputes.map((i: IDeputyHolder) =>
+        assign({}, deputyAttributesMapping(i.depute))
+      )
+    );
 }
 
-export function getDeputy(nom: string) {
-  return getFromUrl(`https://www.nosdeputes.fr/${nom}/json`)
+export function getDeputy(slug: string) {
+  return getFromUrl(`https://www.nosdeputes.fr/${slug}/json`)
     .then((r: IResponse) => r.data)
-    .then((d: {}) => providedNormalizeDeputy(d));
+    .then((d: IDeputyHolder) => deputyAttributesMapping(d.depute));
 }
 
 export default {
---
+++ src/index.test.ts
@@ -0,0 +1,41 @@
+import { getDeputies, getDeputiesInOffice, getDeputy } from "./index";
+
+it("Call getDeputies()", async () => {
+  return getDeputies().then(ds => {
+    expect(ds.length).toBeGreaterThan(0);
+    ds.forEach(d => {
+      expect(d.image15).toBeDefined();
+      expect(d.image30).toBeDefined();
+      expect(d.image60).toBeDefined();
+      expect(d.image120).toBeDefined();
+      expect(d.imageDynamic(50)).toBeDefined();
+      expect(d.nbMandatsTotaux).toBeDefined();
+    });
+  });
+});
+
+it("Call getDeputiesInOffice()", async () => {
+  return getDeputiesInOffice().then(ds => {
+    expect(ds.length).toBeGreaterThan(0);
+    ds.forEach(d => {
+      expect(d.image15).toBeDefined();
+      expect(d.image30).toBeDefined();
+      expect(d.image60).toBeDefined();
+      expect(d.image120).toBeDefined();
+      expect(d.imageDynamic(50)).toBeDefined();
+      expect(d.nbMandatsTotaux).toBeDefined();
+    });
+  });
+});
+
+it("Call getDeputy('patricia-lemoine')", async () => {
+  return getDeputy("patricia-lemoine").then(d => {
+    expect(d).toBeDefined();
+    expect(d.image15).toBeDefined();
+    expect(d.image30).toBeDefined();
+    expect(d.image60).toBeDefined();
+    expect(d.image120).toBeDefined();
+    expect(d.imageDynamic(50)).toBeDefined();
+    expect(d.nbMandatsTotaux).toBeDefined();
+  });
+});
